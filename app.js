bd8f84e92c48d6b6448dd27103b67d7d28e3ae68
Matt <mowen@iq.harvard.edu>
2013-07-30 12:18:57 -0400

    PICTUREPLANE, Colby Drasher, Ardie Petsas

--- app.js
+++ app.js
@@ -20,57 +20,40 @@ app.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: 2592000000 
 
 // svv.im/
 app.get("/", function (req, resp) {
+  var ip = req.headers['X-Forwarded-For'] ? req.headers['X-Forwarded-For'] : req.connection.remoteAddress;
   resp.render("index.ejs");
 });
-//
-// svv.im/
-app.get("/fuck", function (req, resp) {
-  resp.render("fuckit.ejs");
-});
-
 // svv.im/in/...
-app.get("/in/:location", function (req, resp) {
-  resp.render("in.ejs");
-});
-
+app.get("/in/:location", function (req, resp) { }); 
 // svv.im/at/...
 app.get("/at/:venue", function (req, resp) {
   resp.render("at.ejs");
 });
-
 //
 app.get("/ping", function (req, resp) {
   resp.set("Content-Type", "text/plain");
   resp.send("pinged");
 });
-
 //
 app.post("/ping", function (req, resp) {
   resp.set("Content-Type", "text/plain");
   resp.send("pinged");
 });
-
 //
 app.get('/xxx', function (req, resp) {
-  // Respond immediately with nothing
   resp.set('Content-Type', 'text/plain');
-  console.log('okay = ' + req.param("hub.challenge"));
   resp.send(req.param("hub.challenge"));
-
   instagram.getmedia('pool', function (images) {
     io.sockets.send(JSON.stringify(images));
   });
 });
-
-/* POST request to get update from server
+/**
+ * POST request to get update from server
  * Get the request. Afterwards, 
  */
 app.post('/xxx', function (req, resp) {
-  // Respond immediately with nothing
   resp.set('Content-Type', 'text/plain');
-  console.log('okay = ' + req.param("hub.challenge"));
   resp.send(req.param("hub.challenge"));
-
   instagram.getmedia('pool', function (images) {
     io.sockets.send(JSON.stringify(images));
   });
