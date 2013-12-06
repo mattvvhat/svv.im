8e0564631658687b309c6aa4590163d2287088ac
Matt <mowen@iq.harvard.edu>
2013-12-06 15:34:04 -0500

    Xo

---
+++ ir.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+
+var Instagram = require('instagram-node-lib');
+var secrets   = require('./secrets.json');
+
+Instagram.set('client_id',      secrets.client_id);
+Instagram.set('client_secret',  secrets.client_secret);
+
+Instagram.tags.recent({
+  name      : 'yolo',
+  complete  : function (data) {
+    console.log(data);
+  }
+});
